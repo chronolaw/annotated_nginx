32706c0522ddaf2279162b387d17fae0ceb779b8
Chrono Law <jianfeng.luo@chinacache.com>
2016-04-01 16:11:00 +0800

    ngx_thread_pool.h

--- nginx/src/core/ngx_thread_pool.h
+++ nginx/src/core/ngx_thread_pool.h
@@ -1,3 +1,4 @@
+// annotated by chrono since 2016
 
 /*
  * Copyright (C) Nginx, Inc.
@@ -13,7 +14,10 @@
 #include <ngx_core.h>
 #include <ngx_event.h>
 
+// 编译时需使用选项--with-threads
+// thread_pool指令需要在main域配置
 
+// 线程执行的任务结构体，handler是真正被线程执行的函数
 struct ngx_thread_task_s {
     ngx_thread_task_t   *next;
     ngx_uint_t           id;
@@ -27,9 +31,14 @@ typedef struct ngx_thread_pool_s  ngx_thread_pool_t;
 
 
 ngx_thread_pool_t *ngx_thread_pool_add(ngx_conf_t *cf, ngx_str_t *name);
+
+// 根据名字获取线程池
 ngx_thread_pool_t *ngx_thread_pool_get(ngx_cycle_t *cycle, ngx_str_t *name);
 
+// 创建一个线程任务结构体
 ngx_thread_task_t *ngx_thread_task_alloc(ngx_pool_t *pool, size_t size);
+
+// 把任务放入线程池，由线程执行
 ngx_int_t ngx_thread_task_post(ngx_thread_pool_t *tp, ngx_thread_task_t *task);
 
 
