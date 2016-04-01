b14b69152dc64426a423ea74b48a6f87fdcfc4f9
Chrono Law <jianfeng.luo@chinacache.com>
2016-04-01 15:42:21 +0800

    list buf

--- nginx/src/core/ngx_buf.h
+++ nginx/src/core/ngx_buf.h
@@ -1,3 +1,4 @@
+// annotated by chrono since 2016
 
 /*
  * Copyright (C) Igor Sysoev
@@ -13,58 +14,66 @@
 #include <ngx_core.h>
 
 
+// 用于ngx_buf_t，关联任意的数据
 typedef void *            ngx_buf_tag_t;
 
 typedef struct ngx_buf_s  ngx_buf_t;
 
+// 表示一个单块的缓冲区，既可以是内存也可以是文件
+// start和end两个成员变量标记了数据所在内存块的边界
+// 如果内存块是可以修改的，在操作时必须参考这两个成员防止越界
 struct ngx_buf_s {
-    u_char          *pos;
-    u_char          *last;
-    off_t            file_pos;
-    off_t            file_last;
+    u_char          *pos;           //内存数据的起始位置
+    u_char          *last;          //内存数据的结束位置
+    off_t            file_pos;      //文件数据的起始偏移量
+    off_t            file_last;     //文件数据的结束偏移量
 
-    u_char          *start;         /* start of buffer */
-    u_char          *end;           /* end of buffer */
-    ngx_buf_tag_t    tag;
-    ngx_file_t      *file;
+    u_char          *start;         /* start of buffer */   //内存数据的上界
+    u_char          *end;           /* end of buffer */     //内存数据的下界
+    ngx_buf_tag_t    tag;           //void*指针，可以是任意数据
+    ngx_file_t      *file;          //存储数据的文件对象
     ngx_buf_t       *shadow;
 
 
     /* the buf's content could be changed */
-    unsigned         temporary:1;
+    unsigned         temporary:1;   //内存块临时数据，可以修改
 
     /*
      * the buf's content is in a memory cache or in a read only memory
      * and must not be changed
      */
-    unsigned         memory:1;
+    unsigned         memory:1;      //内存块数据，不允许修改
 
     /* the buf's content is mmap()ed and must not be changed */
-    unsigned         mmap:1;
+    unsigned         mmap:1;        //内存映射数据，不允许修改
 
     unsigned         recycled:1;
-    unsigned         in_file:1;
-    unsigned         flush:1;
-    unsigned         sync:1;
-    unsigned         last_buf:1;
-    unsigned         last_in_chain:1;
+    unsigned         in_file:1;     //缓冲区在文件里
+    unsigned         flush:1;       //要求Nginx立即输出本缓冲区
+    unsigned         sync:1;        //要求Nginx同步操作本缓冲区
+    unsigned         last_buf:1;    //最后一块缓冲区
+    unsigned         last_in_chain:1;   //链里的最后一块缓冲区
 
     unsigned         last_shadow:1;
-    unsigned         temp_file:1;
+    unsigned         temp_file:1;       //缓冲区在临时文件里
 
     /* STUB */ int   num;
 };
 
 
+// 把缓冲区块简单地组织为一个单向链表
+// 如果节点是链表的尾节点就必须要把next置为nullptr，表示链表结束
+// ngx_chain_t (ngx_core.h)
 struct ngx_chain_s {
-    ngx_buf_t    *buf;
-    ngx_chain_t  *next;
+    ngx_buf_t    *buf;      //缓冲区指针
+    ngx_chain_t  *next;     //下一个链表节点
 };
 
 
+//创建链表的参数结构
 typedef struct {
-    ngx_int_t    num;
-    size_t       size;
+    ngx_int_t    num;       //缓冲区的数量，即节点数量
+    size_t       size;      //缓冲区的大小
 } ngx_bufs_t;
 
 
@@ -131,9 +140,11 @@ typedef struct {
 #define NGX_CHAIN_ERROR     (ngx_chain_t *) NGX_ERROR
 
 
+// 检查多个标志位，确定缓冲区是否在内存里
 #define ngx_buf_in_memory(b)        (b->temporary || b->memory || b->mmap)
 #define ngx_buf_in_memory_only(b)   (ngx_buf_in_memory(b) && !b->in_file)
 
+// 起控制作用的特殊缓冲区
 #define ngx_buf_special(b)                                                   \
     ((b->flush || b->last_buf || b->sync)                                    \
      && !ngx_buf_in_memory(b) && !b->in_file)
@@ -142,17 +153,23 @@ typedef struct {
     (b->sync                                                                 \
      && !ngx_buf_in_memory(b) && !b->in_file && !b->flush && !b->last_buf)
 
+// 计算缓冲区的大小，会根据是否在内存里使用恰当的指针
 #define ngx_buf_size(b)                                                      \
     (ngx_buf_in_memory(b) ? (off_t) (b->last - b->pos):                      \
                             (b->file_last - b->file_pos))
 
+// 从内存池里分配一块size大小的缓冲区
 ngx_buf_t *ngx_create_temp_buf(ngx_pool_t *pool, size_t size);
+
+// 一次创建多个缓冲区，返回一个连接好的链表
 ngx_chain_t *ngx_create_chain_of_bufs(ngx_pool_t *pool, ngx_bufs_t *bufs);
 
 
+// 直接从内存池创建一个ngx_buf_t结构
 #define ngx_alloc_buf(pool)  ngx_palloc(pool, sizeof(ngx_buf_t))
 #define ngx_calloc_buf(pool) ngx_pcalloc(pool, sizeof(ngx_buf_t))
 
+// 从内存池里获取释放chain
 ngx_chain_t *ngx_alloc_chain_link(ngx_pool_t *pool);
 #define ngx_free_chain(pool, cl)                                             \
     cl->next = pool->chain;                                                  \
