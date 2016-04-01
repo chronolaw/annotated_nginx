20ed04465d7ab34dd93fdde21995ef391c4dd2cf
Chrono Law <jianfeng.luo@chinacache.com>
2016-04-01 16:00:52 +0800

    ngx_murmurhash.h

--- nginx/src/core/ngx_murmurhash.h
+++ nginx/src/core/ngx_murmurhash.h
@@ -1,3 +1,4 @@
+// annotated by chrono since 2016
 
 /*
  * Copyright (C) Igor Sysoev
@@ -13,6 +14,9 @@
 #include <ngx_core.h>
 
 
+// MurmurHash是摘要算法家族里的新成员（发明于2008年），对文本字符串有很好的效果，具有更好的随机性。
+// 原算法可以有一个hash初始值，nginx的实现写死了0,可以修改以适合自己的需要
+// eg: h = seed ^ len;
 uint32_t ngx_murmur_hash2(u_char *data, size_t len);
 
 
