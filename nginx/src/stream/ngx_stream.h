00ea0f4773b8620c931be04a85e1f1f91fe8ce8f
chronolaw <chrono_cpp@me.com>
2016-04-26 13:06:57 +0800

    ngx_stream.h

--- nginx/src/stream/ngx_stream.h
+++ nginx/src/stream/ngx_stream.h
@@ -203,6 +203,7 @@ typedef struct {
 // 类似ngx_http_request_t，表示tcp通信的会话
 // 存储有tcp处理里需要的数据，例如connection、ctx等
 // 缺点是无法扩展，如果增加一个void*成员就好了
+// 可以在ctx数组里利用模块ctx来扩展存储，但还是不太方便
 struct ngx_stream_session_s {
     // 结构体的标志，可以用来识别对象
     uint32_t                signature;         /* "STRM" */
