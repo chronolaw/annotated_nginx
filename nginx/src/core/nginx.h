5ccd17ceb1ab97f63e96365674441c2f8501b81a
chronolaw <chrono_cpp@me.com>
2016-05-17 10:42:08 +0800

    nginx.h => 1.10

--- nginx/src/core/nginx.h
+++ nginx/src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1008001
-#define NGINX_VERSION      "1.8.1"
+#define nginx_version      1010000
+#define NGINX_VERSION      "1.10.0"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
