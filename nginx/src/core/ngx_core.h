136b5448095130dbf65d0871aba0206f5ac0e592
chronolaw <chrono_cpp@me.com>
2016-05-17 10:45:29 +0800

    ngx_core.h => 1.10

--- nginx/src/core/ngx_core.h
+++ nginx/src/core/ngx_core.h
@@ -53,7 +53,9 @@ typedef void (*ngx_connection_handler_pt)(ngx_connection_t *c);
 #include <ngx_shmem.h>
 #include <ngx_process.h>
 #include <ngx_user.h>
+#include <ngx_dlopen.h>
 #include <ngx_parse.h>
+#include <ngx_parse_time.h>
 #include <ngx_log.h>
 #include <ngx_alloc.h>
 #include <ngx_palloc.h>
@@ -71,6 +73,7 @@ typedef void (*ngx_connection_handler_pt)(ngx_connection_t *c);
 #endif
 #include <ngx_radix_tree.h>
 #include <ngx_times.h>
+#include <ngx_rwlock.h>
 #include <ngx_shmtx.h>
 #include <ngx_slab.h>
 #include <ngx_inet.h>
@@ -81,6 +84,7 @@ typedef void (*ngx_connection_handler_pt)(ngx_connection_t *c);
 #endif
 #include <ngx_process_cycle.h>
 #include <ngx_conf_file.h>
+#include <ngx_module.h>
 #include <ngx_open_file_cache.h>
 #include <ngx_os.h>
 #include <ngx_connection.h>
