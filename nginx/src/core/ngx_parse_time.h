2f629b97e0f932e0eee914db66b8bdc13430b388
chronolaw <chrono_cpp@me.com>
2016-05-23 14:34:13 +0800

    ngx_parse_time.h

--- nginx/src/core/ngx_parse_time.h
+++ nginx/src/core/ngx_parse_time.h
@@ -1,3 +1,7 @@
+// annotated by chrono since 2016
+//
+// 由原http目录迁移而来
+// 原文件是ngx_http_parse_time.c
 
 /*
  * Copyright (C) Igor Sysoev
@@ -13,9 +17,11 @@
 #include <ngx_core.h>
 
 
+// 解析http格式日期，不仅供http模块使用
 time_t ngx_parse_http_time(u_char *value, size_t len);
 
 /* compatibility */
+// 使用宏保留了之前版本的名字
 #define ngx_http_parse_time(value, len)  ngx_parse_http_time(value, len)
 
 
