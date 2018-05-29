b669a0346915b34da4f4a0556522a07f12812e29
chronolaw <chrono_cpp@me.com>
2018-05-29 16:58:06 +0800

    ngx_atomic.h

--- nginx/src/os/unix/ngx_atomic.h
+++ nginx/src/os/unix/ngx_atomic.h
@@ -1,3 +1,4 @@
+// annotated by chrono since 2016
 
 /*
  * Copyright (C) Igor Sysoev
@@ -12,6 +13,12 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
+// Linux下通常使用的是gcc atomic
+//#define ngx_atomic_cmp_set(lock, old, set)                                    \
+//    __sync_bool_compare_and_swap(lock, old, set)
+//
+//#define ngx_atomic_fetch_add(value, add)                                      \
+//    __sync_fetch_and_add(value, add)
 
 #if (NGX_HAVE_LIBATOMIC)
 
