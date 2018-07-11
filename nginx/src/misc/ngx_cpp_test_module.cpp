2b8db512c432bcacf69b26f7f53396fe846a8d53
chronolaw <chrono_cpp@me.com>
2018-07-11 10:42:46 +0800

    ngx_cpp_test_module.cpp

--- nginx/src/misc/ngx_cpp_test_module.cpp
+++ nginx/src/misc/ngx_cpp_test_module.cpp
@@ -1,6 +1,11 @@
+// annotated by chrono since 2016
+//
+// 测试Nginx编译C++的兼容性，并不是模块
+// 在auto/modules里搜MISC
 
 // stub module to test header files' C++ compatibility
 
+// 在C++代码里使用nginx头文件需要用extern "C"
 extern "C" {
   #include <ngx_config.h>
   #include <ngx_core.h>
@@ -19,6 +24,9 @@ extern "C" {
 // nginx header files should go before other, because they define 64-bit off_t
 // #include <string>
 
+// 没有更多的功能代码，仅仅是验证编译
+// 但表示可以使用C++开发Nginx模块
+// 不支持C++11特性，必须改auto/make
 
 void ngx_cpp_test_handler(void *data);
 
