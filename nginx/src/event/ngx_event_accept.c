ea68a90bcdc41ac9a6e2590b3bbd5e30c5f26856
chronolaw <chrono_cpp@me.com>
2022-01-26 08:26:45 +0800

    code clean

--- nginx/src/event/ngx_event_accept.c
+++ nginx/src/event/ngx_event_accept.c
@@ -24,7 +24,6 @@ static ngx_int_t ngx_disable_accept_events(ngx_cycle_t *cycle, ngx_uint_t all);
 #if (NGX_HAVE_EPOLLEXCLUSIVE)
 static void ngx_reorder_accept_events(ngx_listening_t *ls);
 #endif
-static void ngx_close_accepted_connection(ngx_connection_t *c);
 
 // 发生了错误，关闭一个连接
 static void ngx_close_accepted_connection(ngx_connection_t *c);
